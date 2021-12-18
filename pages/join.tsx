d86bf666bd67f2c017c0df8acefe7bf8e9fe0256
Jiwon <winning1993@naver.com>
2021-12-18 23:26:48 +0900

    [ADD} 회원가입 Form - regex 적용

--- pages/join.tsx
+++ pages/join.tsx
@@ -1,15 +1,60 @@
 import { NextPage } from "next"
 import Link from "next/link"
-import { useEffect } from "react"
+import { useEffect, useRef, useState } from "react"
 import PageTitle from "../components/PageTItle"
 import SiteMark from "../components/SiteMark"
+import { gql, useMutation, useQuery } from "@apollo/client";
+import { validateEmail, validatePw } from "../config/regexChecks"
+
+
+const JOIN = gql`
+mutation {
+  sayHello
+}
+`
 
 const Join: NextPage = () => {
 
+    const [inputs, setInputs] = useState({
+      email: '',
+      password: '',
+      password2: '',
+    });
+
+    const joinSubmitBtn = useRef();
+
 
     useEffect(() => {
-      console.log("컴포넌트 마운트")
-  }, [])
+      console.log("컴포넌트 마운트");
+      activateJoinBtn();
+  }, [inputs])
+
+    const handleJoinSubmit = () => {
+      
+    }
+
+    const { email, password, password2 } = inputs;
+
+    const onChange = (e) => {
+
+      const { value, name, checked } = e.target;
+      setInputs({
+        ...inputs, // 기존의 input 객체를 복사한 뒤
+        [name]: value, // name 키를 가진 값을 value 로 설정
+      });
+      
+    }
+
+    const activateJoinBtn = () => {
+      console.log(inputs)
+      if(validateEmail(email) && validatePw(password) && password === password2) {
+        joinSubmitBtn.current.className = "w-4/6 self-center p-10 bg-black text-white text-2xl"
+      }
+      else {
+        joinSubmitBtn.current.className = "w-4/6 self-center p-10 bg-gray-400 text-white text-2xl"
+      }
+  
+    }
   
     return (
       <section>
@@ -23,14 +68,14 @@ const Join: NextPage = () => {
       </nav>
       
       <form className="flex flex-col p-6" id="joinForm">
-          <input className="border-2 w-4/6 p-6 self-center" type="email" required placeholder="아이디 또는 이메일" />
-          <input className="border-2 w-4/6 p-6 self-center" type="password" required placeholder="비밀번호" />
-          <input className="border-2 w-4/6 p-6 self-center" type="password" required placeholder="비밀번호 확인" />
+          <input onChange={onChange} name="email" className="border-2 w-4/6 p-6 self-center" type="email" required placeholder="아이디 또는 이메일" />
+          <input onChange={onChange} name="password" className="border-2 w-4/6 p-6 self-center" type="password" required placeholder="비밀번호" />
+          <input onChange={onChange} name="password2" className="border-2 w-4/6 p-6 self-center" type="password" required placeholder="비밀번호 확인" />
 
           <div className="self-center p-6">
           <input className="self-center" type="checkbox" /> <span className="self-center">약관 동의</span>
           </div>
-          <input className="w-4/6 self-center p-10 bg-black text-white text-2xl" type="submit" value="JOIN" />
+          <input ref={joinSubmitBtn} className="w-4/6 self-center p-10 bg-black text-white text-2xl" type="submit" value="JOIN" />
       </form>
   
       <nav className="w-screen flex justify-center list-none">
