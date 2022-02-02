7b9f094df13696c82e7932f1ee04ff3702c05b83
Jiwon <winning1993@naver.com>
2022-02-02 23:06:24 +0900

    [ADD] socket.io 웹소켓 모드만 사용하도록 설정 변경

--- hooks/useWebsocket.tsx
+++ hooks/useWebsocket.tsx
@@ -3,9 +3,10 @@ import { io, Socket } from "socket.io-client";
 import { wsServerUrl } from "../config/urls";
 
 const useWebsocket = () => {
-    const { current: socketIoClient } = useRef<Socket>(io(wsServerUrl))
+    const { current: socketIoClient } = useRef<Socket>(io(wsServerUrl, {transports:['websocket']}))
 
     useEffect(() => {
+    
     }, [])
 
     return [socketIoClient]
