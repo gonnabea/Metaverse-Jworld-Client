33bd146d844c9930dc47a0d3db5df48c90a6c184
jiwon1141 <winning1993@naver.com>
2022-10-15 00:02:08 +0900

    [ADD] vm 변경으로 인한 url 변경

--- config/urls.ts
+++ config/urls.ts
@@ -1,12 +1,12 @@
 const serverUrls = {
     local: "http://localhost:4000/",
-    server: "http://152.70.232.36:4000/",
+    server: "http://146.56.145.39:4000/",
     server_dns: "https://jetaverse.site/"
 }
 
 const wsServerUrls = {
     local: "ws://localhost:4000/",
-    server: "ws://152.70.232.36:4000/",
+    server: "ws://146.56.145.39:4000/",
     server_dns: "wss://jetaverse.site/"
 }
 
